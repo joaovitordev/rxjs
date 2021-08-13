0a56a387c9c042c8c193fe20cad9c09161bd677c
joaovitordev <jv17fc@gmail.com>
2021-08-12 21:38:24 -0300

    basico de funçoes

--- funcoes/funcao_1.js
+++ funcoes/funcao_1.js
@@ -1,1 +1,20 @@
-console.log('teste')
+// Function Declaration
+function bomDia() {
+    console.log('Bom dia');
+}
+
+bomDia();
+
+//Function Expression
+const boaTarde = function () {
+    console.log('Boa tarde');
+}
+
+boaTarde();
+
+function somar(a, b) {
+    return a + b;
+}
+
+const resultado = somar(1, 4);
+console.log(resultado)
